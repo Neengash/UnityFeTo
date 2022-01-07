7647ded27bc033f739abafab8dc667e43916b5c2
Neengash <pepmanas93@gmail.com>
2022-01-07 19:07:58 +0100

    Added editor tools

---
+++ Editor/Editor_InspectorLock.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Reflection;
+using UnityEditor;
+
+public class Editor_InspectorLock
+{
+    [MenuItem("Tools/Toggle Inspector Lock (shortcut) &q")] // Shortcut Alt + q
+    static void SelectLockableInspector()
+    {
+        EditorWindow inspectorToBeLocked = EditorWindow.mouseOverWindow; // "EditorWindow.focusedWindow" can be used instead
+        if (inspectorToBeLocked != null  && inspectorToBeLocked.GetType().Name == "InspectorWindow")
+        {
+            Type type = Assembly.GetAssembly(typeof(UnityEditor.Editor)).GetType("UnityEditor.InspectorWindow");
+            PropertyInfo propertyInfo = type.GetProperty("isLocked");
+            bool value = (bool)propertyInfo.GetValue(inspectorToBeLocked, null);
+            propertyInfo.SetValue(inspectorToBeLocked, !value, null);
+            
+            inspectorToBeLocked.Repaint();
+        }
+    }
+}
