3d16e037db1a148f172b1af038e0f3436110d8ec
Neengash <pepmanas93@gmail.com>
2022-02-12 13:44:30 +0100

    Add Namespace Feto to project classes - excluding editor tools

--- ObjectPool/ObjectPool.cs
+++ ObjectPool/ObjectPool.cs
@@ -3,55 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
-{
-    [SerializeField, Tooltip("Throw exception when calling GetNext of an empty pool")]
-    bool ThrowErrorOnEmpty = true;
+namespace Feto {
+    public class ObjectPool : MonoBehaviour
+    {
+        [SerializeField, Tooltip("Throw exception when calling GetNext of an empty pool")]
+        bool ThrowErrorOnEmpty = true;
 
-    [SerializeField] int poolSize;
-    [SerializeField] PoolableObject prefab;
-    [SerializeField] List<PoolableObject> pool;
+        [SerializeField] int poolSize;
+        [SerializeField] PoolableObject prefab;
+        [SerializeField] List<PoolableObject> pool;
 
-    void Awake() {
-        pool = new List<PoolableObject>();
+        void Awake() {
+            pool = new List<PoolableObject>();
 
-        for (int i = 0; i < poolSize; i++) {
-            CreateElement();
+            for (int i = 0; i < poolSize; i++) {
+                CreateElement();
+            }
         }
-    }
 
-    private PoolableObject CreateElement() {
-        PoolableObject element = Instantiate(prefab);
-        element.setPool(this);
-        element.gameObject.SetActive(false);
-        // pool.Add(element); // poolableObjects are added ondisable
-        return element;
-    }
-
-    public void addToPool(PoolableObject element) {
-        pool.Add(element);
-    }
-
-    public PoolableObject GetNext() {
-        #if UNITY_EDITOR
-        CheckEmptyPoolException();
-        #endif
-        CheckEmptyPool();
-
-        PoolableObject element = pool[0];
-        pool.RemoveAt(0);
-        return element;
-    }
-
-    private void CheckEmptyPoolException() {
-        if (pool.Count == 0 && ThrowErrorOnEmpty) {
-            throw new Exception("Calling GetNext on empty Pool");
+        private PoolableObject CreateElement() {
+            PoolableObject element = Instantiate(prefab);
+            element.setPool(this);
+            element.gameObject.SetActive(false);
+            // pool.Add(element); // poolableObjects are added ondisable
+            return element;
         }
-    }
 
-    private void CheckEmptyPool() {
-        if (pool.Count == 0) {
-            CreateElement();
+        public void addToPool(PoolableObject element) {
+            pool.Add(element);
+        }
+
+        public PoolableObject GetNext() {
+            #if UNITY_EDITOR
+            CheckEmptyPoolException();
+            #endif
+            CheckEmptyPool();
+
+            PoolableObject element = pool[0];
+            pool.RemoveAt(0);
+            return element;
+        }
+
+        private void CheckEmptyPoolException() {
+            if (pool.Count == 0 && ThrowErrorOnEmpty) {
+                throw new Exception("Calling GetNext on empty Pool");
+            }
+        }
+
+        private void CheckEmptyPool() {
+            if (pool.Count == 0) {
+                CreateElement();
+            }
         }
     }
 }
--- Singleton/Singleton.cs
+++ Singleton/Singleton.cs
@@ -1,75 +1,77 @@
 using UnityEngine;
 
-public class Singleton <T> : MonoBehaviour where T : Component
-{
-    private static bool quitting = false;
-    private static T _instance;
+namespace Feto {
+    public class Singleton <T> : MonoBehaviour where T : Component
+    {
+        private static bool quitting = false;
+        private static T _instance;
 
-    public static T Instance {
-        get{
-            if (_instance == null && !quitting) {
-                GameObject go = new GameObject();
-                go.name = typeof(T).Name; // Can remove if next sentence is uncommented
-                //go.hideFlags = HideFlags.HideAndDontSave; // Hide in hierarchy
-                _instance = go.AddComponent<T>();
+        public static T Instance {
+            get{
+                if (_instance == null && !quitting) {
+                    GameObject go = new GameObject();
+                    go.name = typeof(T).Name; // Can remove if next sentence is uncommented
+                    //go.hideFlags = HideFlags.HideAndDontSave; // Hide in hierarchy
+                    _instance = go.AddComponent<T>();
+                }
+                return _instance;
             }
-            return _instance;
         }
-    }
 
-    protected virtual void Awake() {
-        if (_instance == null) {
-            _instance = this as T;
-        } else if (_instance != this) {
-            Destroy(gameObject);
-        }
-    }
-
-    protected virtual void OnDestroy() {
-        if (_instance == this) {
-            _instance = null;
-        }
-    }
-
-    protected void OnApplicationQuit() {
-        quitting = true;
-    }
-}
-
-
-public class SingletonPersistent <T> : MonoBehaviour where T : Component
-{
-    private static bool quitting = false;
-    private static T _instance;
-
-    public static T Instance {
-        get{
-            if (_instance == null && !quitting) {
-                GameObject go = new GameObject();
-                go.name = typeof(T).Name; // Can remove if next sentence is uncommented
-                //go.hideFlags = HideFlags.HideAndDontSave; // Hide in hierarchy
-                _instance = go.AddComponent<T>();
+        protected virtual void Awake() {
+            if (_instance == null) {
+                _instance = this as T;
+            } else if (_instance != this) {
+                Destroy(gameObject);
             }
-            return _instance;
+        }
+
+        protected virtual void OnDestroy() {
+            if (_instance == this) {
+                _instance = null;
+            }
+        }
+
+        protected void OnApplicationQuit() {
+            quitting = true;
         }
     }
 
-    protected virtual void Awake() {
-        if (_instance == null) {
-            _instance = this as T;
-            DontDestroyOnLoad(gameObject);
-        } else if (_instance != this) {
-            Destroy(gameObject);
-        }
-    }
 
-    protected virtual void OnDestroy() {
-        if (_instance == this) {
-            _instance = null;
-        }
-    }
+    public class SingletonPersistent <T> : MonoBehaviour where T : Component
+    {
+        private static bool quitting = false;
+        private static T _instance;
 
-    protected void OnApplicationQuit() {
-        quitting = true;
+        public static T Instance {
+            get{
+                if (_instance == null && !quitting) {
+                    GameObject go = new GameObject();
+                    go.name = typeof(T).Name; // Can remove if next sentence is uncommented
+                    //go.hideFlags = HideFlags.HideAndDontSave; // Hide in hierarchy
+                    _instance = go.AddComponent<T>();
+                }
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake() {
+            if (_instance == null) {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            } else if (_instance != this) {
+                Destroy(gameObject);
+            }
+        }
+
+        protected virtual void OnDestroy() {
+            if (_instance == this) {
+                _instance = null;
+            }
+        }
+
+        protected void OnApplicationQuit() {
+            quitting = true;
+        }
     }
 }
