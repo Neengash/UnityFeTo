2e0e18c37e529246c91311ccacd8a3a399bf2d39
Jaume Riera <jaumerg95@gmail.com>
2022-09-19 13:27:42 +0200

    Add TMP to assembly

--- UI/TMPBlinkScriptable.cs
+++ UI/TMPBlinkScriptable.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TMPBlinkScriptable", menuName = "Scriptables/UnityFeTo/TMPBlinkScriptable", order = 1)]
 public class TMPBlinkScriptable : ScriptableObject
 {
+    [Header("Time properties")]
     public float FadeInTime;
     public float FadeInStayTime;
     public float FadeOutTime;
     public float FadeOutStayTime;
 
+    [Header("Other configuration")]
     public bool loop;
 }
