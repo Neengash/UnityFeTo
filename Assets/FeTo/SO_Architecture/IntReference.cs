61f86677b18f36b12839c31a7797f60f16c98ec8
Neengash <pepmanas93@gmail.com>
2023-04-03 21:40:02 +0200

    Add Int Variable And Reference

---
+++ Assets/FeTo/SO_Architecture/IntReference.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace FeTo.SOArchitecture
+{
+    [Serializable]
+    public class IntReference
+    {
+        public bool UseConstant = true;
+        public int ConstantValue;
+        public IntVariable Variable;
+
+        public IntReference() { }
+
+        public IntReference(int value) {
+            UseConstant = true;
+            ConstantValue = value;
+        }
+
+        public int Value {
+            get { return UseConstant ? ConstantValue : Variable.Value; }
+        }
+
+        public static implicit operator int(IntReference reference) {
+            return reference.Value;
+        }
+
+        public override string ToString() => Value.ToString();
+    }
+}
---
+++ Assets/FeTo/SO_Architecture/IntVariable.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace FeTo.SOArchitecture
+{
+    [CreateAssetMenu(fileName = "IntVariable", menuName = "FeTo/SO_Architecture/IntVariable")]
+    public class IntVariable : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [Multiline]
+        public string DeveloperDescription = "";
+#endif  
+
+        public int Value;
+
+        public void SetValue(int value) {
+            Value = value;
+        }
+
+        public void SetValue(IntVariable value) {
+            Value = value.Value;
+        }
+
+        public void ApplyChange(int amount) {
+            Value += amount;
+        }
+
+        public void ApplyChange(IntVariable amount) {
+            Value += amount.Value;
+        }
+    }
+}
