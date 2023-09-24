3b6516ea7e62988d85a00974541ccaf19e40f9d3
Neengash <pepmanas93@gmail.com>
2023-09-24 18:00:27 +0200

    Refactores GameEventListeners to allow an easier extension

--- SO_Architecture/GameEvents/OnEnableGameEventListeners/OnEnableFloatGameEventListener.cs
+++ SO_Architecture/GameEvents/OnEnableGameEventListeners/OnEnableFloatGameEventListener.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 namespace FeTo.SOArchitecture
 {
     [HelpURL("https://github.com/Neengash/UnityFeTo/tree/FeTo/SO_Architecture/GameEvents#scriptable-object---gameevents")]
-    public class FloatGameEventListener : GameEventListener<float, FloatEvent> { }
+    public class OnEnableFloatGameEventListener : OnEnableGameEventListener<float, FloatEvent> { }
 }
--- SO_Architecture/GameEvents/OnEnableGameEventListeners/OnEnableStringGameEventListener.cs
+++ SO_Architecture/GameEvents/OnEnableGameEventListeners/OnEnableStringGameEventListener.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 namespace FeTo.SOArchitecture
 {
     [HelpURL("https://github.com/Neengash/UnityFeTo/tree/FeTo/SO_Architecture/GameEvents#scriptable-object---gameevents")]
-    public class StringGameEventListener : GameEventListener<string, StringEvent> { }
+    public class OnEnableStringGameEventListener : OnEnableGameEventListener<string, StringEvent> { }
 }
