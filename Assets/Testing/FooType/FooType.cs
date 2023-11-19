2cc1aacf7c87071e410f4b27b9bb07da29c79c30
Neengash <pepmanas93@gmail.com>
2023-11-19 22:57:35 +0100

    Add files for flyweight pattern - custom editor with data from S.O. and monobehaviour

---
+++ Assets/Testing/FooType/FooType.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = ("FooType"), menuName = ("FeToTesting"))]
+public class FooType : ScriptableObject
+{
+    public int foo;
+    public string bar;
+}
